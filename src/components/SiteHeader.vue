865d83e7180d8ab2acb9a89b92057be2bd7c6389
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-11-10 16:38:25 +0100

    nav element

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -13,6 +13,18 @@ export default {
             <div class="logo">
                 <img src="../assets/img/dc-logo.png" alt="">
             </div>
+            <div class="info-navbar">
+                <a href="#">CHARACTERS</a>
+                <a href="#">COMICS</a>
+                <a href="#">MOVIES</a>
+                <a href="#">TV</a>
+                <a href="#">GAMES</a>
+                <a href="#">COLLECTIBLES</a>
+                <a href="#">VIDEOS</a>
+                <a href="#">FANS</a>
+                <a href="#">NEWS</a>
+                <a href="#">SHOP</a>
+            </div>
 
         </nav>
 
