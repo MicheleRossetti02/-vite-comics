7c099bd4f6c42369844b13df500647166a70a24b
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-11-10 16:27:05 +0100

    bootstrap 404

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script >
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import SiteHeader from './components/SiteHeader.vue'
+export default {
+  name: 'App',
+  components: {
+    SiteHeader
+  }
+}
+</script>
+
+<template>
+
+  <site-header>
+
+  </site-header>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,25 @@
+<script >
+export default {
+    name: 'SiteHeader'
+}
+
+</script>
+
+<template>
+    <header id="site-header">
+
+        <nav class="navbar navbar-dark bg-dark">
+            <!-- Navbar content -->
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="">
+            </div>
+
+        </nav>
+
+    </header>
+
+</template>
+
+<style scoped>
+
+</style>
