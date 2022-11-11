713031d9edcb30283028a60fe123efffdd557cd1
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-11-11 18:09:27 +0100

    image complete

---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: "Card",
+    props: {
+        image: String,
+        title: String,
+    }
+}
+</script>
+
+<template>
+    <div class="col my-4">
+        <div class="card ">
+            <div class="card_image">
+                <img :src="image" alt="">
+            </div>
+            <div class="card_text text-white">{{ title }}</div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+img {
+    width: 100%;
+
+}
+</style>
