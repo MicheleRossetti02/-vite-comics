de89c0c10fb64ff717f8387fc5b6d4b77698b920
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-11-11 18:39:16 +0100

    complete

--- src/components/FirstBlock.vue
+++ src/components/FirstBlock.vue
@@ -1,7 +1,18 @@
 <script >
-import Comics from './Comics.vue'
+import ComicsCard from './Card.vue'
+import comics from '../assets/data/dc-comics'
+
 export default {
-    name: 'FirstBlock'
+    name: 'FirstBlock',
+    components: {
+        ComicsCard
+    },
+    data() {
+        return {
+            comics: comics,
+        }
+    },
+
 }
 
 
@@ -15,11 +26,21 @@ export default {
         <div class="main_block">
             <div class=" content">
                 <div class="container ">
-                    <Comics></Comics>
-                    <!-- <div class="row" v-for="comic in comics">
+                    <div class="row row-cols-3 row-cols-sm-4 row-cols-md-6 ">
 
-                    </div> -->
+                        <ComicsCard v-for="comic in comics" :image="comic.thumb" :title="comic.series">
+                        </ComicsCard>
+                        <!-- <Comics></Comics> -->
+                        <!-- <div class="row" v-for="comic in comics">
+    
+                        </div> -->
+                    </div>
+                    <div class="button_load py-3">
+                        <button class="px-5 py-2 text-white">
+                            LOAD MORE
+                        </button>
 
+                    </div>
 
                 </div>
 
@@ -28,3 +49,7 @@ export default {
     </div>
 
 </template>
+
+<style scoped>
+
+</style>
